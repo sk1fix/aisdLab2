9dca9214de8bd12985f9f40073ffbc40e97c914e
sk1fix <agafonovtoha427@gmail.com>
2024-04-29 14:02:52 +0400

    struct and class

--- include/picture.h
+++ include/picture.h
@@ -1,322 +1,59 @@
 #pragma once
-#include <cstdio>
 #include <iostream>
-#include <cstdlib>
-#include <chrono>
-#include <random>
 #include <vector>
+#include <functional>
+#include <stdexcept>
 using namespace std;
-namespace tree {
-	template <typename T>
-	struct Node
-	{
-		T _val;
-		Node* left;
-		Node* right;
-		Node(T val) : _val(val), left(nullptr), right(nullptr) {}
-	};
-
-	template <typename T>
-	class BinarySearchTree
-	{
-	private:
-		Node<T>* root;
-		void printTree(Node<T>* root) {
-			if (root) {
-				printTree(root->left);
-				cout << root->_val << " ";
-				printTree(root->right);
-			}
-		}
-
-		template <typename T>
-		Node<T>* insertTree(Node<T>* root, int key) {
-			if (root == nullptr) {
-				return new Node<T>(key);
-			}
-			if (key < root->_val) {
-				root->left = insertTree(root->left, key);
-			}
-			else if (key > root->_val) {
-				root->right = insertTree(root->right, key);
-			}
-			return root;
-		}
-
-		
-
-		template <typename T>
-		void clear(Node<T>* head) {
-			if (head) {
-				clear(head->left);
-				clear(head->right);
-				delete head;
-			}
-		}
-
-		template <typename T>
-		Node<T>* findMin(Node<T>* node) {
-			Node<T>* current = node;
-			while (current && current->left) {
-				current = current->left;
-			}
-			return current;
-		}
-
-		template <typename T>
-		bool deleteNode(Node<T>*& node, int value) {
-			if (node == nullptr) {
-				return false;
-			}
-			if (value < node->_val) {
-				return deleteNode(node->left, value);
-			}
-			else if (value > node->_val) {
-				return deleteNode(node->right, value);
-			}
-			else {
-				if (node->left == nullptr) {
-					Node<T>* temp = node->right;
-					delete node;
-					node = temp;
-				}
-				else if (node->right == nullptr) {
-					Node<T>* temp = node->left;
-					delete node;
-					node = temp;
-				}
-				else {
-					Node<T>* temp = node->right;
-					while (temp->left) {
-						temp = temp->left;
-					}
-					node->_val = temp->_val;
-					deleteNode(node->right, temp->_val);
-				}
-				return true;
-			}
-		}
-	public:
-		BinarySearchTree() : root(nullptr) {}
-		
-		BinarySearchTree(const int* data) {
-			root = nullptr;
-
-			int l = 0;
-
-			for (size_t i = 0; i < sizeof(data);i++) {
-				insert(data[i]);
-			}
-		}
-
-		BinarySearchTree(const BinarySearchTree& other) {
-			root = nullptr;
-			copyTree(other.root);
-		}
-		
-		template <typename T>
-		void copyTree(Node<T>* node) {
-			if (!node) {
-				insert(node->_val);
-				copyTree(node->left);
-				copyTree(node->right);
-			}
-		}
-
-		bool insert(int key) {
-			root = insertTree(root, key);
-			return true;
-		}
-
-		~BinarySearchTree() {
-			clear(root);
-		}
-
-		void print() {
-			printTree(root);
-		}
-
-		template <typename T>
-		bool contains(T val) {
-			if (!root) {
-				return false;
-			}
-			Node<T>* cur = root;
-			while (cur) {
-				if (cur->_val == val) {
-					return true;
-				}
-				else if (cur->_val < val) {
-					cur = cur->right;
-				}
-				else if (cur->_val > val) {
-					cur = cur->left;
-				}
-			}
-			return false;
-		}
-
-		void erase(int key) {
-			deleteNode(root, key);
-		}
-
-		BinarySearchTree operator=(BinarySearchTree other) {
-			swap(root, other.root);
-			return *this;
-		}
-
-		vector<int> random(int a, int b, size_t n) {
-			vector<int> res;
-			random_device rd;
-			mt19937 gen(rd());
-			uniform_int_distribution<int> distribution(a, b);
-			for (size_t j = 0; j < n; j++) {
-				int x = distribution(gen);
-				res.push_back(x);
-			}
-			return res;
-		}
-
-
-		void fillTreeWithRandomNumbers(int count) {
-			double time_count_vec = 0;
-			double time_count_tree = 0;
-			for (int j = 0; j < 100; j++) {
-				random_device rd;
-				mt19937 gen(rd());
-				uniform_int_distribution<int> dist(-100000, 100000);
-
-				auto start_vec = chrono::high_resolution_clock::now();
-				vector<int> vec = random(-100000, 100000, count);
-				auto end_vec = chrono::high_resolution_clock::now();
-
-				
-				auto start = chrono::high_resolution_clock::now();
-				for (int i = 0; i < count; ++i) {
-					int randomNum = dist(gen);
-					insert(randomNum);
-				}
-				auto end = chrono::high_resolution_clock::now();
-				time_count_tree += chrono::duration<double>(end - start).count();
-				time_count_vec += chrono::duration<double>(end_vec - start_vec).count();
-			}
-			cout << "Time taken to fill the tree with " << count << " random numbers: " << time_count_tree/100 << " seconds" << endl;
-			cout << "Time taken to fill the vec with " << count << " random numbers: " << time_count_vec / 100 << " seconds" << endl;
-		}
-
-		void containsTreeWithRandomNumbers(int count_t) {
-			double time_count_vec = 0;
-			double time_count_tree = 0;
-
-			random_device rd;
-			mt19937 gen(rd());
-			uniform_int_distribution<int> dist(-100000, 100000);
-			for (int i = 0; i < count_t; ++i) {
-				int randomNum = dist(gen);
-				insert(randomNum);
-			}
-			auto start_tree = chrono::high_resolution_clock::now();
-			for (int j = 0; j < 1000; j++) {
-				int randomNum = dist(gen);
-				contains(randomNum);
-			}
-			auto end_tree = chrono::high_resolution_clock::now();
-			vector<int> vec = random(-100000, 100000, count_t);
-			for (int i = 0; i < 1000; i++) {
-				auto start_vec = chrono::high_resolution_clock::now();
-				int cnt = count(vec.begin(), vec.end(), random(-10000, 10000, count_t)[0]);
-				auto end_vec = chrono::high_resolution_clock::now();
-				time_count_vec += chrono::duration<double>(end_vec - start_vec).count();
-			}
-			time_count_tree += chrono::duration<double>(end_tree - start_tree).count();
-			
-			cout << "Time taken to contains the tree with " << count_t << " random numbers: " << time_count_tree / 1000 << " seconds" << endl;
-			cout << "Time taken to contains the vec with " << count_t << " random numbers: " << time_count_vec / 1000 << " seconds" << endl;
-		}
-
-		void insertTreeWithRandomNumbers(int count) {
-			double time_count_vec = 0;
-			double time_count_tree = 0;
-
-			random_device rd;
-			mt19937 gen(rd());
-			uniform_int_distribution<int> dist(-100000, 100000);
-			for (int i = 0; i < count; ++i) {
-				int randomNum = dist(gen);
-				insert(randomNum);
-			}
-			for (int j = 0; j < 1000; j++) {
-				auto start_tree = chrono::high_resolution_clock::now();
-				int randomNum = dist(gen);
-				insert(randomNum);
-				auto end_tree = chrono::high_resolution_clock::now();
-				erase(randomNum);
-				time_count_tree += chrono::duration<double>(end_tree - start_tree).count();
-			}
-			for (int i = 0; i < 1000; i++) {
-				vector<int> vec = random(-100000, 100000, count);
-				auto start_vec = chrono::high_resolution_clock::now();
-				int randomNum = dist(gen);
-				vec.push_back(randomNum);
-				auto end_vec = chrono::high_resolution_clock::now();
-				time_count_vec += chrono::duration<double>(end_vec - start_vec).count();
-			}
-			cout << "Time taken to insert the tree with " << count << " random numbers: " << time_count_tree / 1000 << " seconds" << endl;
-			cout << "Time taken to insert the vec with " << count << " random numbers: " << time_count_vec / 1000 << " seconds" << endl;
-		}
-
-		void eraseTreeWithRandomNumbers(int count) {
-			double time_count_vec = 0;
-			double time_count_tree = 0;
-
-			random_device rd;
-			mt19937 gen(rd());
-			uniform_int_distribution<int> dist(-100000, 100000);
-			for (int i = 0; i < count; ++i) {
-				int randomNum = dist(gen);
-				insert(randomNum);
-			}
-			for (int j = 0; j < 1000; j++) {
-				auto start_tree = chrono::high_resolution_clock::now();
-				int randomNum = dist(gen);
-				erase(randomNum);
-				auto end_tree = chrono::high_resolution_clock::now();
-				insert(randomNum);
-				time_count_tree += chrono::duration<double>(end_tree - start_tree).count();
-				
-			}
-			for (int i = 0; i < 1000; i++) {
-				vector<int> vec = random(-100000, 100000, count);
-				auto start_vec = chrono::high_resolution_clock::now();
-				int randomNum = dist(gen);
-				for (size_t j = 0; j < count; j++) {
-					if (vec[j] == randomNum) {
-						vec.erase(vec.begin() + j);
-						break;
-					}
-				}
-				auto end_vec = chrono::high_resolution_clock::now();
-				time_count_vec += chrono::duration<double>(end_vec - start_vec).count();
-			}
-			cout << "Time taken to erase the tree with " << count << " random numbers: " << time_count_tree / 1000 << " seconds" << endl;
-			cout << "Time taken to erase the vec with " << count << " random numbers: " << time_count_vec / 1000 << " seconds" << endl;
-		}
-	};
-
-
-	template <typename T>
-	vector<int> findDuplicates(const vector<int>& vec) {
-		vector<int> duplicates;
-		BinarySearchTree<int> temp(vec.data());
-		for (size_t i = 0; i < vec.size(); ++i) {
-			if (temp.contains(vec[i])) {
-				temp.erase(vec[i]);
-			}
-			else if (!(count(duplicates.begin(), duplicates.end(), vec[i])))
-				duplicates.push_back(vec[i]);
-		}
-		return duplicates;
-	}
-
-	
+namespace Hash {
+    template<typename K, typename T>
+    struct HashElement {
+        K key;
+        T value;
+        bool filled;
+        HashElement() : filled(false) {}
+    };
+    template<typename K, typename T>
+    class HashTable {
+    private:
+        vector<HashElement<K, T>> table;
+        int capacity;
+        unsigned int hashFunction(K key) {
+            unsigned int temp = std::hash<K>{}(key);
+            return (temp * 2654435761) % capacity;
+        }
+    public:
+        HashTable(int size) : capacity(size) {
+            table.resize(capacity);
+        }
+        void insert(K key, T value) {
+            unsigned int index = hashFunction(key);
+            while (table[index].filled) {
+                index = (index + 1) % capacity;
+            }
+            table[index].key = key;
+            table[index].value = value;
+            table[index].filled = true;
+        }
+        T search(K key) {
+            unsigned int index = hashFunction(key);
+            while (table[index].filled) {
+                if (table[index].key == key) {
+                    return table[index].value;
+                }
+                index = (index + 1) % capacity;
+            }
+            throw out_of_range("Key not found in the hash table.");
+        }
+        void remove(K key) {
+            unsigned int index = hashFunction(key);
+            while (table[index].filled) {
+                if (table[index].key == key) {
+                    table[index].filled = false;
+                    return;
+                }
+                index = (index + 1) % capacity;
+            }
+            throw out_of_range("Key not found in the hash table.");
+        }
+    };
 }
