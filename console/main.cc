8f9356f187e82f9af60f77a648460e78db1fb766
sk1fix <agafonovtoha427@gmail.com>
2024-05-11 19:50:14 +0400

    count, contains, and other

--- console/main.cc
+++ console/main.cc
@@ -1,20 +1,31 @@
 #pragma once
 #include<picture.h>
 using namespace Hash;
+using namespace std;
 int main()
 {
-    HashTable<int, std::string> ht(10);
+    HashTable<int, string> ht(10);
     ht.insert(1, "One");
     ht.insert(2, "Two");
-
-    std::cout << ht.search(1) << std::endl;
-    std::cout << ht.search(2) << std::endl;
-    ht.remove(1);
+    ht.insert(26, "Twgriho");
+    ht.insert(26, "vksj");
+    ht.insert(3, "dbn");
+    ht.insert(84, "Twgriho");
+    ht.insert(8, "dbn");
+    ht.insert(45, "Twgriho");
+    ht.insert(8, "dbn");
+    ht.insert(9, "Twgriho");
+    ht.print();
+    cout << ht.contains("dbn") << endl;
+    cout << ht.count(8) << endl;
+    cout << ht.search(1) << endl;
+    cout << ht.search(2) << endl;
+    ht.erase(1);
     try {
-        std::cout << ht.search(1) << std::endl;
+        cout << ht.search(1) << endl;
     }
-    catch (const std::out_of_range& e) {
-        std::cerr << e.what() << std::endl;
+    catch (const out_of_range& e) {
+        cerr << e.what() << endl;
     }
     return 0;
 }
--- include/picture.h
+++ include/picture.h
@@ -10,7 +10,8 @@ namespace Hash {
         K key;
         T value;
         bool filled;
-        HashElement() : filled(false) {}
+        HashElement() : key(K()), value(T()), filled(false) {}
+        HashElement(K _key, T _value) : key(_key), value(_value), filled(false) {}
     };
     template<typename K, typename T>
     class HashTable {
@@ -18,11 +19,11 @@ namespace Hash {
         vector<HashElement<K, T>> table;
         int capacity;
         size_t hashFunction(const K& key) {
-            float i = 0.5938475;
+            float i = 0.59384;
             size_t temp = 0;
-            if constexpr (is_same<K, string>) {
+            if constexpr (is_same_v<K, string>) {
                 for (auto c : key) {
-                    temp += static_cast<int>(c);
+                    temp += int(c);
                 }
                 return static_cast<size_t>((temp % table.size() + static_cast<size_t>(i)) % table.size());
             }
@@ -30,12 +31,30 @@ namespace Hash {
                 return static_cast<size_t>((key % table.size() + static_cast<size_t>(i)) % table.size());
             }
         }
+        void copyHash(const HashTable& other) {
+            
+        }
     public:
         HashTable(int size) : capacity(size) {
             table.resize(capacity);
         }
+        HashTable(const HashTable& other) {
+            capacity = other.capacity;
+            table = other.table;
+        }
+        ~HashTable() {
+            table.clear();
+            capacity = 0;
+        }
+        HashTable& operator=(const HashTable& other) {
+            if (this != &other) {
+                capacity = other.capacity;
+                table = other.table;
+            }
+            return *this;
+        }
         void insert(K key, T value) {
-            unsigned int index = hashFunction(key);
+            size_t index = hashFunction(key);
             while (table[index].filled) {
                 index = (index + 1) % capacity;
             }
@@ -43,8 +62,21 @@ namespace Hash {
             table[index].value = value;
             table[index].filled = true;
         }
+        bool contains(T value) {
+            for (auto c : table) {
+                if (c.value == value) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        void print() {
+            for (size_t i = 0; i < table.size(); i++) {
+                cout << table[i].key << " : " << table[i].value << endl;
+            }
+        }
         T search(K key) {
-            unsigned int index = hashFunction(key);
+            size_t index = hashFunction(key);
             while (table[index].filled) {
                 if (table[index].key == key) {
                     return table[index].value;
@@ -53,8 +85,8 @@ namespace Hash {
             }
             throw out_of_range("Key not found in the hash table.");
         }
-        void remove(K key) {
-            unsigned int index = hashFunction(key);
+        void erase(K key) {
+            size_t index = hashFunction(key);
             while (table[index].filled) {
                 if (table[index].key == key) {
                     table[index].filled = false;
@@ -63,6 +95,16 @@ namespace Hash {
                 index = (index + 1) % capacity;
             }
             throw out_of_range("Key not found in the hash table.");
+        }
+        int count(K key) {
+            /*size_t index = hashFunction(key);*/
+            size_t res = 0;
+            for (auto c : table) {
+                if (c.key == key) {
+                    res++;
+                }
+            }
+            return res;
         }
     };
 }
